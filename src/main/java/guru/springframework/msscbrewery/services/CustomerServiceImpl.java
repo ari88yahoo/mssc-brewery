2ccb3b38ea926e040f5fee9e80573610e2f2bbdd
ari88yahoo <chakravarti.arijit@yahoo.com>
2024-12-20 19:37:24 +0530

    Ari feature (#1)
    * Changes related to  Assignment 1.
    
    * put and post impl method added
    
    * Api versioning, Customer Api addl method added

---
+++ src/main/java/guru/springframework/msscbrewery/services/CustomerServiceImpl.java
@@ -0,0 +1,39 @@
+package guru.springframework.msscbrewery.services;
+
+import guru.springframework.msscbrewery.web.model.CustomerDto;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Slf4j
+@Service
+public class CustomerServiceImpl implements CustomerService {
+    @Override
+    public CustomerDto getCustomerById(UUID customerId) {
+        return CustomerDto.
+                builder()
+                .id(UUID.randomUUID())
+                .name("EY")
+                .build();
+    }
+
+    @Override
+    public CustomerDto saveNewCustomer(CustomerDto customerDto) {
+        return CustomerDto.builder()
+                .id(UUID.randomUUID())
+                .build();
+    }
+
+    @Override
+    public void updateCustomer(UUID customerId, CustomerDto customerDto) {
+        //todo IMPL
+        log.debug("Updating Customer....");
+    }
+
+    @Override
+    public void deleteByCustomerId(UUID customerId) {
+        log.debug("Deleting Customer.....");
+    }
+
+}
