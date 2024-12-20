7fe11b42277ea8cd8526a0e704a28a9491b9f093
arijitchakravarti <chakravarti.arijit@yahoo.com>
2024-12-20 19:19:23 +0530

    Api versioning, Customer Api addl method added

--- src/main/java/guru/springframework/msscbrewery/services/CustomerServiceImpl.java
+++ src/main/java/guru/springframework/msscbrewery/services/CustomerServiceImpl.java
@@ -1,11 +1,12 @@
 package guru.springframework.msscbrewery.services;
 
 import guru.springframework.msscbrewery.web.model.CustomerDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
-
+@Slf4j
 @Service
 public class CustomerServiceImpl implements CustomerService {
     @Override
@@ -16,4 +17,23 @@ public class CustomerServiceImpl implements CustomerService {
                 .name("EY")
                 .build();
     }
+
+    @Override
+    public CustomerDto saveNewCustomer(CustomerDto customerDto) {
+        return CustomerDto.builder()
+                .id(UUID.randomUUID())
+                .build();
+    }
+
+    @Override
+    public void updateCustomer(UUID customerId, CustomerDto customerDto) {
+        //todo IMPL
+        log.debug("Updating Customer....");
+    }
+
+    @Override
+    public void deleteByCustomerId(UUID customerId) {
+        log.debug("Deleting Customer.....");
+    }
+
 }
